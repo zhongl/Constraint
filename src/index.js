36be28f4f5791bf9f69d83840452d58b973665e9
zhongl <zhong.lunfu@gmail.com>
2026-08-17 10:49:14 +0800

    upgrade three r103 to r104

--- src/index.js
+++ src/index.js
@@ -59,6 +59,7 @@ function init() {
     _renderer = new THREE.WebGLRenderer({
         antialias : true
     });
+    _renderer.debug.checkShaderErrors = true;
     _renderer.shadowMap.type = THREE.PCFSoftShadowMap;
     _renderer.shadowMap.enabled = true;
     document.body.appendChild(_renderer.domElement);
