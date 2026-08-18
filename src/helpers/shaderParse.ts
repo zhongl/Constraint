94f15112466fc14b5ee4c246d4a552ea24ab46db
zhongl <zhong.lunfu@gmail.com>
2026-08-18 12:21:21 +0800

    refactor: migrate foundational modules to TypeScript

--- src/helpers/shaderParse.ts
+++ src/helpers/shaderParse.ts
@@ -5,43 +5,43 @@ var threeChunkRegExp = /\/\/\s?chunk\(\s?(\w+)\s?\);/g;
 var glslifyBugFixRegExp = /(_\d+_\d+)(_\d+_\d+)+/g;
 var glslifyGlobalRegExp = /GLOBAL_VAR_(.+)(_\d+_\d+)+/g;
 
-var _chunkReplaceObj;
+let _chunkReplaceObj: Record<string, string>;
 
-function _storeChunkReplaceParse(shader) {
+function _storeChunkReplaceParse(shader: string): string {
     _chunkReplaceObj = {};
     return shader.replace(threeChunkReplaceRegExp, _storeChunkReplaceFunc);
 }
 
-function _threeChunkParse(shader) {
+function _threeChunkParse(shader: string): string {
     return shader.replace(threeChunkRegExp, _replaceThreeChunkFunc);
 }
 
-function _glslifyBugFixParse(shader) {
+function _glslifyBugFixParse(shader: string): string {
     return shader.replace(glslifyBugFixRegExp, _returnFirst);
 }
 
-function _glslifyGlobalParse(shader) {
+function _glslifyGlobalParse(shader: string): string {
     return shader.replace(glslifyGlobalRegExp, _returnFirst);
 }
 
-function _storeChunkReplaceFunc(a, b, c) {
+function _storeChunkReplaceFunc(_a: string, b: string, c: string): string {
     _chunkReplaceObj[b.trim()] = c;
     return '';
 }
 
-function _replaceThreeChunkFunc(a, b) {
-    var str = THREE.ShaderChunk[b] + '\n';
+function _replaceThreeChunkFunc(_a: string, b: string): string {
+    var str = THREE.ShaderChunk[b as keyof typeof THREE.ShaderChunk] + '\n';
     for(var id in _chunkReplaceObj) {
         str = str.replace(id, _chunkReplaceObj[id]);
     }
     return str;
 }
 
-function _returnFirst(a, b) {
+function _returnFirst(_a: string, b: string): string {
     return b;
 }
 
-function parse(shader) {
+function parse(shader: string): string {
     shader = _storeChunkReplaceParse(shader);
     shader = _threeChunkParse(shader);
     shader = _glslifyBugFixParse(shader);
