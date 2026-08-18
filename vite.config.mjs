8bf32896abaf3f82856d8864a7a0d59010d66021
zhongl <zhong.lunfu@gmail.com>
2026-08-18 15:51:46 +0800

    build: consume effect package dist in development

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,34 +1,8 @@
-import path from 'node:path';
 import { defineConfig } from 'vite';
-import glslify from 'glslify';
-
-function bundleShader(file) {
-    return glslify.file(file, { basedir: path.dirname(file) });
-}
-
-function glslifyShader() {
-    return {
-        name: 'glslify-shader',
-        enforce: 'pre',
-        async load(id) {
-            var file = id.split('?')[0];
-            if(!/\.(glsl|frag|vert)$/.test(file)) return null;
-
-            var source = await bundleShader(file);
-            return {
-                code: 'export default ' + JSON.stringify(source) + ';',
-                map: null
-            };
-        }
-    };
-}
 
 export default defineConfig({
     base: './',
     publicDir: 'app',
-    plugins: [
-        glslifyShader()
-    ],
     build: {
         outDir: 'dist',
         emptyOutDir: true,
